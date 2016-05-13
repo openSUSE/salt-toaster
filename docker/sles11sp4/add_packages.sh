ef41c1772e92fcfc7dd80a8a20404f73bba03f07
Mihai Dinca <mdinca@suse.de>
2016-05-13 13:18:39 +0200

    Clean toaster-sles11sp4 docker files

--- docker/sles11sp4/add_packages.sh
+++ docker/sles11sp4/add_packages.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 set -e
 
-# make sure the package repository is up to date
-zypper --non-interactive --gpg-auto-import-keys ref
-
-# Packages required to run salt-minion
-zypper -n in  --no-recommends iproute2 \
-                              python \
-                              cron \
-                              sysconfig \
-                              python-openssl \
-                              postfix \
-                              psmisc \
-                              udev \
-                              make \
-                              python-mock \
-                              python-pip \
-                              python-pytest \
-                              python-unittest2 \
-                              python-salt-testing \
-                              net-tools \
-                              bind-utils
-
-# required for unit tests install with recommends
-zypper -n in quilt tar
-
-zypper -n in vim less
+zypper -n in  --no-recommends python-pytest
 
 zypper -n in python-devel
