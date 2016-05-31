f418eb650ac73f9fe51162a8592fb862027e8085
Mihai Dinca <mdinca@suse.de>
2016-05-31 10:59:55 +0200

    Simplify minion keys states assertions

--- assertions.py
+++ assertions.py
@@ -1,29 +1,27 @@
-import re
+import json
 import shlex
 import subprocess
 from config import SALT_KEY_CMD
 
 
-def has_expected_state(expected_state, mapping, env):
-    assert expected_state in mapping
+STATES_MAPPING = dict(unaccepted='minions_pre', accepted='minions')
+
+
+def get_keys_status(env):
     cmd = shlex.split(SALT_KEY_CMD.format(**env))
     cmd.append("-L")
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env)
     output, unused_err = process.communicate()
-    return mapping[expected_state].search(output) is not None
+    return json.loads(output)
 
 
 def assert_minion_key_state(env, expected_state):
-    STATES_MAPPING = dict(
-        unaccepted=re.compile("Unaccepted Keys:(\n.+)*\n{HOSTNAME}".format(**env)),
-        accepted=re.compile("Accepted Keys:(\n.+)*\n{HOSTNAME}".format(**env))
-    )
-    assert has_expected_state(expected_state, STATES_MAPPING, env)
+    assert expected_state in STATES_MAPPING
+    status = get_keys_status(env)
+    assert env['HOSTNAME'] in status[STATES_MAPPING[expected_state]]
 
 
 def assert_proxyminion_key_state(env, expected_state):
-    STATES_MAPPING = dict(
-        unaccepted=re.compile("Unaccepted Keys:(\n.+)*\n{PROXY_ID}".format(**env)),
-        accepted=re.compile("Accepted Keys:(\n.+)*\n{PROXY_ID}".format(**env))
-    )
-    assert has_expected_state(expected_state, STATES_MAPPING, env)
+    assert expected_state in STATES_MAPPING
+    status = get_keys_status(env)
+    assert env['PROXY_ID'] in status[STATES_MAPPING[expected_state]]
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ TIME_LIMIT = 30
 SALT_MASTER_START_CMD = "salt-master -c {SALT_ROOT}"
 SALT_MINION_START_CMD = "salt-minion -l debug -c {SALT_ROOT}"
 SALT_PROXYMINION_START_CMD = "salt-proxy -l debug -c {SALT_ROOT} --proxyid={PROXY_ID}"
-SALT_KEY_CMD = "salt-key -c {SALT_ROOT}"
+SALT_KEY_CMD = "salt-key -c {SALT_ROOT} --output json"
 SALT_CALL = "salt -l debug -c {SALT_ROOT} {HOSTNAME}"
 SALT_PROXY_CALL = "salt -l debug -c {SALT_ROOT} {PROXY_ID}"
 START_PROXY_SERVER = "python -m tests.proxy_server {PROXY_SERVER_PORT}"
