db1fc0434a4858de4064c7b9d1cee7d13ee95335
Mihai Dinca <mdinca@suse.de>
2016-06-03 10:44:03 +0200

    Change test parameters computation

--- tests/test_pkg_compare.py
+++ tests/test_pkg_compare.py
@@ -24,8 +24,7 @@ def get_versions():
         ['0.2~beta2-1', '0.2-1', '-1']
     ]
     major, minor = get_suse_release()
-    VERSIONS += POST_SLE12 if major >= 12 else PRE_SLE12
-    return VERSIONS
+    return VERSIONS + POST_SLE12 if major >= 12 else VERSIONS + PRE_SLE12
 
 
 @pytest.mark.parametrize("ver1,ver2,expected", get_versions())
