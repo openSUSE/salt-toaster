0f289fc55556719587c2b48265c3151b35bc0cb6
Mihai Dinca <mdinca@suse.de>
2016-06-17 10:11:42 +0200

    Cleaning

--- config.py
+++ config.py
@@ -1,12 +1,5 @@
-import os
-
-
 TIME_LIMIT = 120
-SALT_MASTER_START_CMD = "salt-master -c {SALT_ROOT}"
-SALT_MINION_START_CMD = "salt-minion -l debug -c {SALT_ROOT}"
-SALT_PROXYMINION_START_CMD = "salt-proxy -l debug -c {SALT_ROOT} --proxyid={PROXY_ID}"
-SALT_KEY_CMD = "salt-key -c {SALT_ROOT} --output json"
-SALT_CALL = "salt -l debug -c {SALT_ROOT} {HOSTNAME}"
-SALT_PROXY_CALL = "salt -l debug -c {SALT_ROOT} {PROXY_ID} --output json"
-START_PROXY_SERVER = "python -m tests.proxy_server {PROXY_SERVER_PORT}"
-WHEEL_CONFIG = dict(user='apiuser', password='linux')
+WHEEL_CONFIG = {
+    'user': 'apiuser',
+    'password': 'linux'
+}
--- tests/factories.py
+++ tests/factories.py
@@ -1,17 +1,23 @@
 import os
-import re
-import json
 import yaml
 import string
 import factory
 import factory.fuzzy
 from docker import Client
+from models import ContainerModel, MasterModel, MinionModel
 
 
 class BaseFactory(factory.Factory):
 
     class Meta:
         strategy = factory.BUILD_STRATEGY
+
+
+class DockerClientFactory(factory.StubFactory):
+
+    @classmethod
+    def stub(cls, **kwargs):
+        return Client(base_url='unix://var/run/docker.sock')
 
 
 class ImageFactory(factory.StubFactory):
@@ -40,13 +46,6 @@ class ImageFactory(factory.StubFactory):
             for item in output:
                 print item.values()[0]
         return obj
-
-
-class DockerClientFactory(factory.StubFactory):
-
-    @classmethod
-    def stub(cls, **kwargs):
-        return Client(base_url='unix://var/run/docker.sock')
 
 
 class SaltConfigFactory(BaseFactory):
@@ -123,33 +122,6 @@ class ContainerConfigFactory(BaseFactory):
         exclude = ['image_obj', 'salt_config']
 
 
-class ContainerModel(dict):
-
-    def run(self, command):
-        cmd_exec = self['docker_client'].exec_create(
-            self['config']['name'], cmd=command)
-        output = self['docker_client'].exec_start(cmd_exec['Id'])
-        return output
-
-    def get_suse_release(self):
-        info = dict()
-        content = self.run('cat /etc/SuSE-release')
-        for line in content.split('\n'):
-            match = re.match('([a-zA-Z]+)\s*=\s*(\d+)', line)
-            if match:
-                info.update([[match.group(1), int(match.group(2))]])
-        return info
-
-    def get_os_release(self):
-        content = self.run('cat /etc/os-release')
-        return dict(
-            filter(
-                lambda it: len(it) == 2,
-                [it.replace('"', '').strip().split('=') for it in content.split('\n')]
-            )
-        )
-
-
 class ContainerFactory(BaseFactory):
 
     docker_client = None
@@ -167,26 +139,6 @@ class ContainerFactory(BaseFactory):
         data = obj['docker_client'].inspect_container(obj['config']['name'])
         obj['ip'] = data['NetworkSettings']['IPAddress']
         return obj
-
-
-class MasterModel(dict):
-
-    def salt_key_raw(self, *args):
-        command = ['salt-key']
-        command.extend(args)
-        command.append('--output=json')
-        return self['container'].run(' '.join(command))
-
-    def salt_key(self, *args):
-        return json.loads(self.salt_key_raw(*args))
-
-    def salt_key_accept(self, minion_id):
-        return self.salt_key_raw('-a', minion_id, '-y')
-
-    def salt(self, minion_id, salt_command, *args):
-        docker_command = "salt {0} {1} --output=json -l quiet".format(
-            minion_id, salt_command, ' '.join(args))
-        return json.loads(self['container'].run(docker_command))
 
 
 class MasterFactory(BaseFactory):
@@ -209,15 +161,6 @@ class MasterFactory(BaseFactory):
         )
         docker_client.exec_start(res['Id'])
         return obj
-
-
-class MinionModel(dict):
-
-    def salt_call(self, salt_command, *args):
-        docker_command = "salt-call {0} {1} --output=json -l quiet".format(
-            salt_command, ' '.join(args)
-        )
-        return json.loads(self['container'].run(docker_command))['local']
 
 
 class MinionFactory(BaseFactory):
