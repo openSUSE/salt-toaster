396adb2c7613d77272e4524430ad610df107f463
Mihai Dinca <mdinca@suse.de>
2016-06-17 09:44:26 +0200

    Eliminate docker subfolders

--- docker/sles12/testing/add_repositories.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-# Use your credentials for the 'nu.novell' domain within the URL, in case required
-zypper ar -f http://download.opensuse.org/repositories/systemsmanagement:/saltstack:/testing/SLE_12/ "salt"
