f722ab02de27c2abc656bcc5f9ac4dba77d7054e
Mihai Dinca <mdinca@suse.de>
2016-06-27 15:14:37 +0200

    Install missing salttesting with pip

--- docker/bin/install_salt.sh
+++ docker/bin/install_salt.sh
@@ -3,3 +3,5 @@ set -e
 zypper --non-interactive in salt-master salt-minion salt-proxy
 zypper --non-interactive in --oldpackage test-package=42:0.0
 zypper --non-interactive up zypper libzypp
+
+pip install salttesting
