4a4ae8bc8e67d3cdf8c9b9419dafdcf0860738af
Mihai Dinca <mdinca@suse.de>
2016-06-27 13:49:39 +0200

    Eliminate Basis-Devel and install just make

--- docker/bin/add_packages.sh
+++ docker/bin/add_packages.sh
@@ -4,6 +4,6 @@ set -e
 # make sure the package repository is up to date
 zypper --non-interactive --gpg-auto-import-keys ref
 
-zypper -n in --no-recommends python-devel python-pip
+zypper -n in --no-recommends python-devel python-pip make
 pip install pytest
 
--- docker/bin/install_salt.sh
+++ docker/bin/install_salt.sh
@@ -1,4 +1,5 @@
 set -e
+
 zypper --non-interactive in salt-master salt-minion salt-proxy
 zypper --non-interactive in --oldpackage test-package=42:0.0
 zypper --non-interactive up zypper libzypp
