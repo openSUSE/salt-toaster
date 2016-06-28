2980c30a8de6cbdf5d22bb269c16f8c5ad499ba8
Mihai Dinca <mdinca@suse.de>
2016-06-28 09:40:00 +0200

    Fix build output (dots on one line)

--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+import re
 import argparse
 from utils import build_docker_image
 
@@ -6,8 +7,17 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--nocache', action='store_true', default=False)
     args = parser.parse_args()
-    for item in build_docker_image(nocache=args.nocache):
-        print item.values()[0]
+    content = ''
+    stream = build_docker_image(nocache=args.nocache)
+    for item in stream:
+        buff = item.get('stream', item.get('status'))
+
+        if not content or re.search('.+\[[. ]*$', content):
+            content += buff
+
+        if not re.search('.+\[[. ]*$', content):
+            print(content)
+            content = ''
 
 
 if __name__ == '__main__':
