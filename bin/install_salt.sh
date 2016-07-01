238bf6a7cf2d7ba7bbf3a47eea46a90fb5051ad8
Mihai Dinca <mdinca@suse.de>
2016-07-01 22:26:31 +0200

    Eliminate rpm-build

--- bin/install_salt.sh
+++ bin/install_salt.sh
@@ -5,7 +5,7 @@ if [ "$DEVEL" == "true" ]
         docker/bin/prepare_devel.sh
         pip install -e $SALT_REPO_MOUNTPOINT
     else
-        zypper -n install quilt rpmbuild
+        zypper -n install quilt
         docker/bin/install_salt.sh
         zypper --non-interactive source-install -D salt
         bin/unpack_salt.sh
