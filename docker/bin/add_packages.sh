782525fa1aa7770db5f08272483df403af6ab5ee
Mihai Dinca <mdinca@suse.de>
2016-08-03 15:56:07 +0200

    Add bind-utils to sles to fix missing dig

--- docker/bin/add_packages.sh
+++ docker/bin/add_packages.sh
@@ -4,6 +4,6 @@ set -e
 # make sure the package repository is up to date
 zypper --non-interactive --gpg-auto-import-keys ref
 
-zypper -n in --no-recommends python-devel python-pip make
+zypper -n in --no-recommends python-devel python-pip make bind-utils
 pip install pytest mock
 
