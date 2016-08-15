26be9570236842d151412b74224d415725b89b00
Mihai Dinca <mdinca@suse.de>
2016-08-15 15:14:10 +0200

    Create missing salt config files

--- docker/bin/prepare_devel.sh
+++ docker/bin/prepare_devel.sh
@@ -3,3 +3,7 @@ zypper --non-interactive in netcat swig gcc-c++ libopenssl-devel
 zypper --non-interactive in --oldpackage test-package=42:0.0
 pip install M2Crypto pyzmq PyYAML pycrypto msgpack-python jinja2 psutil
 pip install rpdb
+
+mkdir -p /etc/salt
+touch /etc/salt/master
+touch /etc/salt/minion
