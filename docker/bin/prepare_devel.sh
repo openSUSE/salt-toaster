5e797d5d016820178ca806db32ffa68eecba9b7f
Mihai Dinca <mdinca@suse.de>
2016-08-16 11:48:18 +0200

    Clean salt DEVEL requirements list

--- docker/bin/prepare_devel.sh
+++ docker/bin/prepare_devel.sh
@@ -1,15 +1,5 @@
 set -e
-zypper --non-interactive in \
-    swig \
-    gcc-c++ \
-    libopenssl-devel \
-    python-m2crypto \
-    python-pycrypto \
-    python-msgpack-python \
-    python-PyYAML \
-    python-Jinja2 \
-    python-psutil \
-    python-pyzmq
+zypper --non-interactive in swig gcc-c++ libopenssl-devel python-m2crypto
 
 zypper --non-interactive in --oldpackage test-package=42:0.0
 zypper --non-interactive in netcat
