c313feac4066f99a3efc62f4e84f1c7116626ddc
Mihai Dinca <mdinca@suse.de>
2016-08-18 13:08:58 +0200

    Clean before and after yumdownload on rhel

--- bin/rhel_unpack_salt.sh
+++ bin/rhel_unpack_salt.sh
@@ -10,9 +10,10 @@ fi
 mkdir -p /usr/src/packages/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 echo '%_topdir /usr/src/packages' > ~/.rpmmacros
 echo '%_tmppath %{_topdir}/tmp' >> ~/.rpmmacros
+rm salt-*.src.rpm
 yumdownloader --source salt
 rpm -ivh salt-*.src.rpm
-rm *.rpm
+rm salt-*.src.rpm
 rpmbuild -bp /usr/src/packages/SPECS/salt.spec
 mkdir -p $SALT_SOURCES/src/
 ln -s /usr/src/packages/BUILD/salt-* $SALT_SOURCES/src/
