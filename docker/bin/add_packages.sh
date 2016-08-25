5f5faa67b4f6cbff9b1807150b5264921fce49b1
Mihai Dinca <mdinca@suse.de>
2016-08-25 18:25:35 +0200

    Install missing gcc in containers

--- docker/bin/add_packages.sh
+++ docker/bin/add_packages.sh
@@ -4,5 +4,5 @@ set -e
 # make sure the package repository is up to date
 zypper --non-interactive --gpg-auto-import-keys ref
 
-zypper -n in --no-recommends python-devel python-pip make bind-utils
+zypper -n in --no-recommends python-devel python-pip make bind-utils gcc-c++
 pip install pytest mock==1.0.0 timelib
