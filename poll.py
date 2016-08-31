862cd4ff893e83a416de11b2f6507c9beafea5ee
Mihai Dinca <mdinca@suse.de>
2016-08-31 10:21:08 +0200

    Poll

--- poll.py
+++ poll.py
@@ -11,14 +11,17 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--url', required=True, dest='url', type=str)
+    parser.add_argument('--etag-file', required=True, dest='etagfile', type=str)
 
     params = parser.parse_args()
 
-    etag_file = py.path.local('etag')
+    etag_file = py.path.local(params.etagfile)
 
-    etag = '""'
-    if etag_file.exists():
-        etag = etag_file.read('rb')
+    if not etag_file.exists():
+        response = requests.get(params.url, stream=True)
+        etag_file.write(response.headers['ETag'])
+
+    etag = etag_file.read('rb')
 
     response = requests.get(
         params.url, headers={'If-None-Match': '{0}'.format(etag)})
