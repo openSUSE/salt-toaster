7ba6731e7dc792a98cdb1034b0cbae325865abcf
Mihai Dinca <mdinca@suse.de>
2016-11-10 15:40:10 +0100

    Solve salt-testing unit xfailures

--- docker/bin/add_packages.sh
+++ docker/bin/add_packages.sh
@@ -5,4 +5,4 @@ set -e
 zypper --non-interactive --gpg-auto-import-keys ref
 
 zypper -n in --no-recommends python-devel python-pip make bind-utils gcc-c++ python-apache-libcloud
-pip install pytest mock==1.0.0 timelib
+pip install pytest mock==1.0.0 timelib boto
