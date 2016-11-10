2ce668bf85eb801269771c657c53482f5bb71390
Mihai Dinca <mdinca@suse.de>
2016-11-10 11:36:44 +0100

    Test apply latest twice using sls files

--- tests/test_states.py
+++ tests/test_states.py
@@ -6,49 +6,61 @@ from saltcontainers.factories import MinionFactory, MasterFactory
 
 
 @pytest.fixture(scope='module')
-def module_config(request, docker_client, salt_root, file_root, pillar_root):
-    fake = Faker()
-
-    master_name = 'master_{0}_{1}'.format(fake.word(), fake.word())
-    master_image = request.config.getini('IMAGE')
-    master_config = dict(
-        container__config__name=master_name,
-        container__config__docker_client=docker_client,
-        container__config__image=master_image,
-        container__config__salt_config__tmpdir=salt_root,
-        container__config__salt_config__conf_type='master',
-        container__config__salt_config__config={
-            'base_config': {
-                'pillar_roots': {'base': [pillar_root]},
-                'file_roots': {'base': [file_root]}}},
-        container__config__salt_config__sls={
-            'latest': {'latest-state': {'pkg.latest': [{'name': 'postfix'}]}}})
-
-    minion_name = 'minion_{0}_{1}'.format(fake.word(), fake.word())
-    minion_image = master_image or request.config.getini('IMAGE')
-    minion_config = dict(
-        container__config__name=minion_name,
-        container__config__docker_client=docker_client,
-        container__config__image=minion_image,
-        container__config__salt_config__tmpdir=salt_root,
-        container__config__salt_config__conf_type='minion',
-        container__config__salt_config__config={'base_config': {}})
-
+def module_config(request):
     return {
         'masters': [
             {
-                'config': master_config,
-                'minions': [
-                    {'config': minion_config}
-                ]
+                'config': {
+                    'container__config__salt_config__sls': {
+                        'oldest': {
+                            'oldest-version': {
+                                'pkg.installed': [
+                                    {
+                                        'name': 'test-package',
+                                        'version': '42:0.0-3.1'
+                                    }
+                                ]
+                            }
+                        },
+                        'latest': {
+                            'include': ['oldest'],
+                            'latest-version': {
+                                'pkg.latest': [
+                                    {'name': 'test-package'},
+                                    {'require': [{'pkg': 'oldest-version'}]}
+                                ]
+                            },
+                        },
+                        'latest-again': {
+                            'include': ['latest'],
+                            'latest-version-again': {
+                                'pkg.latest': [
+                                    {'name': 'test-package'},
+                                    {'require': [{'pkg': 'latest-version'}]}
+                                ]
+                            }
+                        }
+                    }
+                },
+                'minions': [{'config': {}}, {'config': {}}]
             }
         ]
     }
 
 
 def test_pkg_latest_version(setup):
-    fixtures, config = setup
-    master = fixtures[config['masters'][0]['id']]['fixture']
-    minion = fixtures[master['id']]['minions'][0]
-    resp = master.salt(minion['id'], 'state.apply latest')
-    assert resp[minion['id']]['pkg_|-latest-state_|-postfix_|-latest']['result']
+    config, initconfig = setup
+    master = config['masters'][0]
+    minion = master['minions'][0]
+    resp = master['fixture'].salt(minion['id'], 'state.apply latest')
+    assert resp[minion['id']][
+        'pkg_|-latest-version_|-test-package_|-latest']['result'] is True
+
+
+def test_pkg_latest_version_already_installed(setup):
+    config, initconfig = setup
+    master = config['masters'][0]
+    minion = master['minions'][1]
+    resp = master['fixture'].salt(minion['id'], 'state.apply latest-again')
+    assert resp[minion['id']][
+        'pkg_|-latest-version_|-test-package_|-latest']['result'] is True
