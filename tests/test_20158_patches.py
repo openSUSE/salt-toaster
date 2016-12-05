92d1944cad4cf416983a66edff0bdaf4d7ba6646
Bo Maryniuk <bo@suse.de>
2016-12-05 18:39:15 +0100

    Add rsync test

---
+++ tests/test_20158_patches.py
@@ -0,0 +1,38 @@
+import re
+import pytest
+import pdb
+
+pytestmark = pytest.mark.usefixtures("master", "minion")
+
+@pytest.fixture(scope='module')
+def module_config(request):
+    return {
+        'masters': [
+            {
+                'config': {
+                    'container__config__salt_config__sls': {
+                        'rsync': 'tests/sls/rsync.sls',
+                    }
+                },
+                'minions': [{'config': {}}, {'config': {}}]
+            }
+        ]
+    }
+
+
+def _minion(setup, cmd):
+    '''
+    Call salt on current minion
+    '''
+    config, initconfig = setup
+    master = config['masters'][0]
+    minion = master['minions'][0]
+    return master['fixture'].salt(minion['id'], cmd)[minion['id']]
+
+
+@pytest.mark.tags('sles')
+def test_rsync_port(setup):
+    resp = _minion(setup, "state.apply rsync")
+    assert resp['pkg_|-rsyncpackage_|-rsync_|-installed']['result']
+    assert resp['rsync_|-/tmp_|-/tmp_|-synchronized']['result']
+
