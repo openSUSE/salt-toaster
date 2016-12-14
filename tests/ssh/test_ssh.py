7de23ed35d981f80f2663fd922edd4f4c57df280
Bo Maryniuk <bo@suse.de>
2016-12-14 18:13:27 +0100

    Add test to echo some data over the network to a forwarded port

--- tests/ssh/test_ssh.py
+++ tests/ssh/test_ssh.py
@@ -1,4 +1,6 @@
+import socket
 import pytest
+import multiprocessing
 from tests.common import GRAINS_EXPECTATIONS
 
 
@@ -105,3 +107,37 @@ def test_ssh_pkg_remove_sles(master):
     out = master.salt_ssh("pkg.remove test-package")
     assert out.get('test-package', {}).get('old')
     assert not out.get('test-package', {}).get('new')
+
+
+def socket_server(port, queue):
+    '''
+    Opens a local socket at specified port
+    '''
+    skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    skt.bind(('127.0.0.1', port))
+    skt.listen(1)
+    conn, addr = skt.accept()
+    data = conn.recv(1024)
+    conn.close()
+    queue.put('{"%s": "%s"}' % (addr[0], data))
+
+
+def test_ssh_port_forwarding(master):
+    '''
+    Test SSH port forwarding feature.
+    PR: https://github.com/saltstack/salt/pull/38021
+    '''
+    local_port = 8888
+    queue = multiprocessing.Queue()
+    srv = multiprocessing.Process(target=socket_server, args=(local_port, queue,))
+    srv.start()
+    
+    
+    netcat = master.salt_ssh("cmd.run 'zypper --non-interactive in netcat-openbsd'")
+    whichnc = master.salt_ssh("cmd.run 'which nc'")
+    isnetsend = master.salt_ssh("cmd.run 'file /salt-toaster/tests/scripts/netsend.sh'")
+    xxx = 'N/A'
+    if srv.is_alive():
+        xxx = master.salt_ssh("--remote-port-forwards=9999:127.0.0.1:8888 cmd.run '/salt-toaster/tests/scripts/netsend.sh foobar 9999'")
+
+    import pdb;pdb.set_trace()
