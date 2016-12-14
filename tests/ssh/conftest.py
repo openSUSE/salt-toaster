022784437732236b06c10be0944e09ec5bd17337
Bo Maryniuk <bo@suse.de>
2016-12-14 13:03:41 +0100

    Move output to the variable for a better debuggings

--- tests/ssh/conftest.py
+++ tests/ssh/conftest.py
@@ -70,7 +70,7 @@ def master(setup):
     def _cmd(master, cmd):
         SSH = "salt-ssh -i --out json --key-deploy --passwd {0} {1} {{0}}".format(
             PASSWORD, TARGET_ID)
-        return json.loads(
-            master['container'].run(SSH.format(cmd))).get(TARGET_ID)
+        out = master['container'].run(SSH.format(cmd))
+        return json.loads(out).get(TARGET_ID)
     master.salt_ssh = partial(_cmd, master)
     return master
