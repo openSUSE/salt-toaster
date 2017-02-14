1488fbb57ede9a3d724d60f9fd24963c30683ecd
Bo Maryniuk <bo@suse.de>
2017-02-14 14:26:22 +0100

    Add preliminary test for Salt package update. Depends on https://github.com/dincamihai/pytest-salt-containers/pull/1/files

--- tests/test_pkg.py
+++ tests/test_pkg.py
@@ -58,6 +58,25 @@ def timezone(request, minion):
     request.addfinalizer(partial(finalizer, minion))
 
 
+def test_salt_upgrade(minion):
+    ex = minion["container"].run
+    ex("rm /etc/zypp/repos.d/salt.repo")
+    ex("zypper --non-interactive rm salt salt-minion")
+    ex("/usr/bin/cp /salt-toaster/tests/sysconfigs/salt-products.repo /etc/zypp/repos.d/")
+    ex("zypper --non-interactive in salt-minion")
+    ex("rm /etc/zypp/repos.d/salt-products.repo")
+    ex("/usr/bin/cp /salt-toaster/tests/sysconfigs/salt-next.repo /etc/zypp/repos.d/")
+    ex("zypper ref")
+    minion.stop()
+    ex("zypper --non-interactive up salt-minion")
+    ex("cp /etc/salt/minion.rpmsave /etc/salt/minion")
+    minion.start()
+
+    import time
+    time.sleep(5)
+    assert minion.salt_call('test.ping')
+
+
 @pytest.mark.xfail
 def test_pkg_info_install_date(minion, timezone):
     res = minion.salt_call('pkg.info_installed', 'test-package')
