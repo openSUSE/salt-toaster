3bda806020fa782eab2c04c05589a9aad6012b6b
Mihai Dinca <mdinca@suse.de>
2017-06-12 10:52:16 +0200

    Add syndic test

---
+++ tests/test_syndic.py
@@ -0,0 +1,52 @@
+import pytest
+from utils import retry
+
+
+@pytest.fixture(scope='module')
+def masters(syndics, minions):
+    return [
+        {
+            'config': {
+                "container__config__salt_config__extra_configs": {
+                    "syndic": {
+                        "order_masters": True
+                    },
+                }
+            },
+            'syndics': syndics,
+            'minions': [minions[0]]
+        }
+    ]
+
+
+@pytest.fixture(scope='module')
+def syndics(minions):
+    return [
+        {'minions': [minions[1], minions[2]]},
+        {'minions': [minions[3], minions[4]]}
+    ]
+
+
+@pytest.fixture(scope='module')
+def minions():
+    return 5 * [{}]
+
+
+@pytest.fixture(scope='module')
+def module_config(masters):
+    return {'masters': masters}
+
+
+@pytest.mark.timeout(0)
+def test_syndic_test_ping(setup, masters, syndics, minions):
+
+    status = masters[0]['fixture'].salt_run('manage.status')
+
+    assert syndics[0]['id'] in status['down']
+    assert syndics[1]['id'] in status['down']
+
+    assert minions[0]['id'] in status['up']
+    assert minions[1]['id'] in status['up']
+    assert minions[2]['id'] in status['up']
+    assert minions[3]['id'] in status['up']
+    assert minions[4]['id'] in status['up']
