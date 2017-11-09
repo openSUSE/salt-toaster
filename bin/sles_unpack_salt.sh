e59d96aab476f4c9759078271eb31da9ecb135d5
Mihai Dinca <mdinca@suse.de>
2017-11-09 09:28:00 +0100

    Fixes for sles15

--- bin/sles_unpack_salt.sh
+++ bin/sles_unpack_salt.sh
@@ -11,7 +11,7 @@ mkdir -p /usr/src/packages/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 echo '%_topdir /usr/src/packages' > ~/.rpmmacros
 echo '%_tmppath %{_topdir}/tmp' >> ~/.rpmmacros
 zypper -n si salt
-rpmbuild -bp /usr/src/packages/SPECS/salt.spec
+rpmbuild --without zsh_completion --without docs --without test -bp /usr/src/packages/SPECS/salt.spec
 mkdir -p $SALT_SOURCES/src/
 ln -s /usr/src/packages/BUILD/salt-* $SALT_SOURCES/src/
 exit 0
