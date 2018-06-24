7a288540ca1a1e633c3ab12fa207772d99c74d21
MalloZup <dmaiocchi@suse.com>
2018-06-24 20:52:02 +0200

    add all tags

--- tasks.py
+++ tasks.py
@@ -3,10 +3,14 @@
 from invoke import task
 
 @task(help={'suse': "run susetests"})
-def leap15(c, suse=False):
+def leap15(c, suse=True, saltstack.unit=False, saltstack.integration=False):
     """
     run leap15 tests
     """
     c.run("docker pull dmaiocchi/leap15-salt-toaster")
     if suse:
         c.run("sandbox/bin/pytest -c ./configs/suse.tests/leap15/products.cfg ./tests")
+    if saltstack.unit:
+        c.run("sandbox/bin/pytest -c ./configs/saltstack.unit/leap15/products.cfg ./tests")
+    if saltstac.integration:
+        c.run("sandbox/bin/pytest -c ./configs/saltstack.integration/leap15/products.cfg ./tests")
