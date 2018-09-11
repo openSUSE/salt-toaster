0566a1313d732fe20fd2426c7fc60918f313fe14
Mihai Dinca <mdinca@suse.de>
2018-09-11 17:37:39 +0200

    Comment out zypper execution to allow running on Ubuntu

--- tests/ssh/conftest.py
+++ tests/ssh/conftest.py
@@ -32,6 +32,6 @@ def container(request, salt_root):
         config__image=request.config.getini('MINION_IMAGE') or request.config.getini('IMAGE'),
         config__salt_config=None,
         ssh_config={'user': 'root', 'password': 'admin123'})
-    obj.run('zypper --non-interactive rm salt')  # Remove salt from the image!!
+    # obj.run('zypper --non-interactive rm salt')  # Remove salt from the image!!
     request.addfinalizer(obj.remove)
     return obj
