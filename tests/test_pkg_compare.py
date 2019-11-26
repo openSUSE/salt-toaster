544bbdb413cfaa2d7af7e8f82e8b789ca9d087a9
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2019-11-26 13:58:57 +0000

    Prevent test_pkg_compare to fail on SLE15SP2

--- tests/test_pkg_compare.py
+++ tests/test_pkg_compare.py
@@ -15,7 +15,7 @@ def pytest_generate_tests(metafunc):
         ['1:0.2-1', '0.2-1', 1],
     ]
     if not tags.isdisjoint(
-        {'sles15sp1', 'sles15', 'sles12', 'sles12sp1', 'sles12sp2', 'sles12sp3', 'leap42sp1', 'rhel6', 'rhel7', 'ubuntu'}
+        {'sles15sp2', 'sles15sp1', 'sles15', 'sles12', 'sles12sp1', 'sles12sp2', 'sles12sp3', 'leap42sp1', 'rhel6', 'rhel7', 'ubuntu'}
     ):
         VERSIONS += [
             ['0.2-1', '0.2~beta1-1', 1],
